5e2ff018a9d7f8ee7973685bf54b6d39105e7c69
garick161 <sergey.gagarin1985@gmail.com>
2023-10-07 16:56:32 +0300

    Скрипт для семплирования фотографии для дальнейшей разметки. Отбирает фотографии, сохраняет train, test датафреймы

---
+++ scripts/sampling_images.py
@@ -0,0 +1,52 @@
+import shutil
+import pandas as pd
+import numpy as np
+import os
+from typing import List
+
+
+def get_sample_indexes(path_to_df: str, random_state: int = 2023, reduce_coef: float = 1.0) -> List:
+    df = pd.read_csv(path_to_df)
+    rs = np.random.RandomState(random_state)
+
+    thresh = int((len(df) / df['sub_class'].nunique()) * reduce_coef)
+    idx_list = []
+
+    for cls in df['sub_class'].unique():
+        temp_df = df[df['sub_class'] == cls]
+        if len(temp_df) <= thresh:
+            idx_list += list(temp_df.index.values)
+        else:
+            idx_list += list(rs.choice(temp_df.index.values, thresh, replace=False))
+
+    return idx_list
+
+
+def copy_img_for_labeling(path_to_df: str, idx: List, path_to_save: str):
+    if not os.path.exists(path_to_save):
+        os.mkdir(path_to_save)
+    df = pd.read_csv(path_to_df)
+
+    files_list = df.loc[idx]['path_to_img'].to_list()
+    for path in files_list:
+        shutil.copy(os.path.join(path), os.path.join(path_to_save))
+
+    return None
+
+
+def save_train_test_df(path_to_df: str, idx: List, class_name: str):
+    df = pd.read_csv(path_to_df)
+    train = df[df.index.isin(idx)]
+    test = df[~df.index.isin(idx)]
+    train.to_csv(f'../dataframes/{class_name}_train.csv', index=False)
+    test.to_csv(f'../dataframes/{class_name}_test.csv', index=False)
+    return None
+
+
+if __name__ == '__main__':
+    path = '../dataframes/bridge_down_with_subclass.csv'
+    path_to_save = '../images_for_labeling'
+    class_name = 'bridge_down'
+    idx = get_sample_indexes(path_to_df=path, reduce_coef=0.6)
+    copy_img_for_labeling(path_to_df=path, idx=idx, path_to_save=path_to_save)
+    save_train_test_df(path_to_df=path, idx=idx, class_name=class_name)
