081a5c3f3d7075234ed41e56894260a98e196ee4
garick161 <sergey.gagarin1985@gmail.com>
2023-10-09 18:43:39 +0300

    Модификации в точке входа, создание датафрейма

--- scripts/get_classification_report.py
+++ scripts/get_classification_report.py
@@ -1,11 +1,10 @@
-from check_result_without_logs import detect_class
+from check_result_model_without_logs import detect_class
+from sklearn.metrics import classification_report
 import pandas as pd
 from tqdm import tqdm
-from sklearn.metrics import classification_report
 
 
-def predict(path_to_df: str) -> pd.DataFrame:
-    df = pd.read_csv(path_to_df)
+def predict(df: pd.DataFrame) -> pd.DataFrame:
     predict_list = []
     for video in tqdm(df['path_to_video']):
         result = detect_class(video)
@@ -14,12 +13,27 @@ def predict(path_to_df: str) -> pd.DataFrame:
     return df
 
 
-def print_classfication_report(df: pd.DataFrame):
+def print_classfication_report(df: pd.DataFrame, name: str):
+    print(f'{name}')
     print(classification_report(y_true=df['true_label'], y_pred=df['predict']))
 
 
 if __name__ == '__main__':
-    path = '../dataframes/all_train.csv'
-    # path = '../dataframes/all_test.csv'
-    result = predict(path_to_df=path)
-    print_classfication_report(result)
+    print("Введите 'train' или 'test'")
+    sample = input()
+
+    bridge_down = pd.read_csv(f'../dataframes/bridge_down_{sample}.csv')
+    bridge_up = pd.read_csv(f'../dataframes/bridge_up_{sample}.csv')
+    no_action = pd.read_csv(f'../dataframes/no_action_{sample}.csv')
+    train_in_out = pd.read_csv(f'../dataframes/train_out_{sample}.csv')
+
+    columns = ['video_name', 'true_label', 'path_to_video']
+
+    all_data = pd.concat((bridge_down[columns],
+                          bridge_up[columns],
+                          no_action[columns],
+                          train_in_out[columns]),
+                         axis=0, ignore_index=True)
+
+    result = predict(df=all_data)
+    print_classfication_report(df=result, name=sample)
