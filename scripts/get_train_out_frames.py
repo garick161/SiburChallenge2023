9f70a780921c95a6f8cdfaedab5ad281abbe59fb
garick161 <sergey.gagarin1985@gmail.com>
2023-10-09 17:05:16 +0300

    Изменения под новый формат датафрейма

--- scripts/get_train_out_frames.py
+++ scripts/get_train_out_frames.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import cv2
+from tqdm import tqdm
 import os
 from contrast_increase import contrast_increase
 
@@ -9,11 +10,10 @@ def frames_for_labeling(path_to_df: str):
     """Функция для получения фреймов из видео для дальнейшей разметки"""
     df = pd.read_csv(path_to_df)
 
-    for video_path in df['path_to_video']:
-        name = video_path.rsplit('\\')[-1].split('.')[0]
-        cap = cv2.VideoCapture(video_path)
+    for row in tqdm(range(len(df))):
+        cap = cv2.VideoCapture(df['path_to_video'][row])
         count_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
+        img_path = (df['path_to_img'][row]).rsplit('.', 1)
         # берем 5 равномерно распределенных кадров
         frames = np.linspace(1, count_frames, 5, dtype='int32')
         for n_frame in frames:
@@ -22,23 +22,31 @@ def frames_for_labeling(path_to_df: str):
 
             # выполняем повышение контрастности для удаления засвеченности
             frame_new = contrast_increase(frame)
-            cv2.imwrite(os.path.join('../images_for_labeling', f'{name}_{n_frame}.jpg'), frame_new)
+
+            cv2.imwrite(f'_{n_frame}.'.join(img_path), frame_new)
 
 
-def save_train_test_df(path_to_video: str, random_state: int = 2023, test_ratio: float = 0.2):
-    df = pd.DataFrame()
-    path_list = []
-    for entry in os.scandir(path_to_video):
-        if entry.is_file() and entry.name.endswith('.mp4'):
-            path_list.append(entry.path)
-    df['path_to_video'] = path_list
+def save_train_test_df(df: pd.DataFrame, random_state: int = 2023, test_ratio: float = 0.2):
     test = df.sample(frac=test_ratio, replace=False, random_state=random_state)
     train = df[~df.index.isin(test.index.values)]
+    print(f'Всего видеофайлов: {len(df)}')
+    print(f'train: {len(train)}')
+    print(f'test: {len(test)}')
     test.to_csv('../dataframes/train_out_test.csv', index=False)
     train.to_csv('../dataframes/train_out_train.csv', index=False)
 
 
 if __name__ == '__main__':
-    path_video_dir = '../prepair_dataset/train/train_in_out'
-    save_train_test_df(path_to_video=path_video_dir)
+    path_to_video = '../prepair_dataset/train/train_in_out'
+    path_to_save = '../images_for_labeling'
+    if not os.path.exists(path_to_save):
+        os.mkdir(path_to_save)
+
+    #  Подготовка структуры датафрейма
+    df = pd.DataFrame()
+    df['video_name'] = [f for f in os.listdir(path_to_video) if f.endswith('.mp4')]
+    df['true_label'] = 'train_in_out'
+    df['path_to_video'] = df['video_name'].apply(lambda name: f'{path_to_video}/{name}')
+    df['path_to_img'] = df['video_name'].apply(lambda name: f"{path_to_save}/{name.split('.')[0]}.jpg")
+    save_train_test_df(df=df)
     frames_for_labeling(path_to_df='../dataframes/train_out_train.csv')
