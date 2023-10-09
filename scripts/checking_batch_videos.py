158cdd95c632cddb43f59780ee6d1f98d94f5b18
garick161 <sergey.gagarin1985@gmail.com>
2023-10-09 17:40:41 +0300

    Убрано создание датафрейма, добавлена документация

--- scripts/checking_batch_videos.py
+++ scripts/checking_batch_videos.py
@@ -1,28 +1,14 @@
-from check_result_yolo8n import detect_class
-import os
-from loguru import logger
+from check_result_model_with_logs import detect_class
+from collections import defaultdict
 from datetime import datetime as dt
 import time
+from loguru import logger
 from tqdm import tqdm
-from collections import defaultdict
-import pandas as pd
-
-classes = ['bridge_down_type_1', 'bridge_down_type_2', 'bridge_up', 'bridge_up_type_1', 'bridge_up_type_2',
-           'coupling', 'plate_type_1', 'plate_type_2', 'track']
-log_file_name = dt.now().strftime('%H-%M_%d-%m-%Y')
-path_to_log_file = os.path.join('../logs', f'{log_file_name}.log')
-logger.remove()  # Чтобы не выводилось в консоль, а писалось только в файл
-logger.add(path_to_log_file, format='{time:HH:mm:ss} {message}')
+import os
 
 
-df = pd.DataFrame()
-
-
-class_list_detected = []
-path_list = []
-
-
-def help_func():
+def check_video():
+    """Функция запускает определение класса на видео и администрирует логирование"""
     count_video = 0
     count_class_detected = defaultdict(int)
     for entry in tqdm(os.scandir(path_to_dir)):
@@ -31,23 +17,30 @@ def help_func():
             logger.info('#######################################\n')
             logger.info(f'{entry.path}\n')
             start_time = time.time()
-            stat_matx, result = detect_class(entry.path)
-            global class_list_detected
-            class_list_detected.append(result)
-            global path_list
-            path_list.append(entry.path)
+            stat_matx, result = detect_class(entry.path)  # Результат работы модели
             count_class_detected[result] += 1
             logger.info(f'Operation:{result}\n')
-            logger.info("Process time: %s seconds\n#######################################\n\n" % (time.time() - start_time))
+            logger.info(
+                "Process time: %s seconds\n#######################################\n\n" % (time.time() - start_time))
     else:
         logger.info(f'{count_class_detected}\ncount_video: {count_video}\n{dict(zip(classes, stat_matx))}')
 
 
 if __name__ == '__main__':
+    print("Введите имя тестируемого класса")
     cls = input()
+
     path_to_dir = f'../prepair_dataset/train/{cls}'
-    help_func()
-    df['path'] = path_list
-    df['predict'] = class_list_detected
-    df['true_label'] = cls
-    df.to_csv(f'../dataframes/{cls}_df.csv', index=False)
+    path_to_logs = '../logs'
+    if not os.path.exists(path_to_logs):
+        os.mkdir(path_to_logs)
+
+    #  настройка файла логирования
+    classes = ['bridge_down_1', 'bridge_down_2', 'bridge_up_1', 'bridge_up_2', 'coupling', 'plate_type_1',
+               'plate_type_2', 'track']
+    log_file_name = dt.now().strftime('%H-%M_%d-%m-%Y')
+    path_to_log_file = f'{path_to_logs}/{log_file_name}.log'
+    logger.remove()  # Чтобы не выводилось в консоль, а писалось только в файл
+    logger.add(path_to_log_file, format='{time:HH:mm:ss} {message}')
+
+    check_video()
