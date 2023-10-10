34c9259966a8df712beb1a85ab54acf79f9beeee
garick161 <sergey.gagarin1985@gmail.com>
2023-10-10 17:55:07 +0300

    Перемещение файла

--- scripts/check_one_video.py
+++ scripts/check_one_video.py
@@ -2,7 +2,7 @@ from show_results_model import get_frames_with_bb
 
 
 if __name__ == '__main__':
-    weights_path = '../scripts/weights_ver5.pt'
-    get_frames_with_bb(video_path='../prepair_dataset/train/train_in_out\\24b468923c4ae72b.mp4',
+    weights_path = 'weights_ver5.pt'
+    get_frames_with_bb(video_path='../prepair_dataset/train/bridge_down/790f22429c3b917a.mp4',
                        path_to_dir='../problem_frames',
                        path_to_weights=weights_path)
